c7ec45ff2f328e02a75003724b2349efc29c7b5c
Jaikhlang Brahma <jaikhlang@gmail.com>
2019-11-06 00:21:51 +0530

    new commit

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -25,10 +25,9 @@
               <span class="block">February 7th, 8th & 9th of 2020 | CIT Kokrajhar BTAD Assam India</span>
             </div>
           </div>
-
         </div>
 
-        <div class="text-center">
+        <div class="text-center py-2">
           <h6 class="font-semibold text-sm text-gray-800">Hosted by Department of Humanities & Social Sciences <br>& Center for Linguistics and Cultural Studies, CIT Kokrajhar</h6>
           <a href="{{ url('/') }}" class="text-decoration-none text-gray-800">http://neils.co.in</a>
         </div>
@@ -39,32 +38,32 @@
         <table class="table-auto border-none w-full mb-0">
           <tbody>
             <tr>
-              <td class="border-dashed border-b border-t border-r px-4 py-2">Invoice Id:</td>
-              <td class="border-dashed border-b border-t px-4 py-2">{{ $user->regno }}</td>
+              <td class="border-red-500 border-dashed border-b border-t px-4 py-2">Invoice Id:</td>
+              <td class="border-red-500 border-dashed border-b border-t px-4 py-2">{{ $user->regno }}</td>
             </tr>
-            <tr class="bg-gray-100">
-              <td class="border-dashed border-b border-r px-4 py-2">Name:</td>
-              <td class="border-dashed border-b px-4 py-2">{{ $user->firstname.' '.$user->lastname }}</td>
-            </tr>
-            <tr>
-              <td class="border-b border-r px-4 py-2">Email:</td>
-              <td class="border-b px-4 py-2">{{ $user->email }}</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border-b border-r px-4 py-2">Phone:</td>
-              <td class="border-b px-4 py-2">{{ $user->phone }}</td>
+            <tr class="">
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Name:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">{{ $user->firstname.' '.$user->lastname }}</td>
             </tr>
             <tr>
-              <td class="border-b border-r px-4 py-2">Purpose:</td>
-              <td class="border-b px-4 py-2">Registration for NEILS Conference 2020.</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Email:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">{{ $user->email }}</td>
+            </tr>
+            <tr class="">
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Phone:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">{{ $user->phone }}</td>
             </tr>
             <tr>
-              <td class="border-r px-4 py-2">Amount:</td>
-              <td class="px-4 py-2" colspan="2">{{ $user->payment->amount }} INR (Registration Fee)</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Purpose:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Registration for NEILS Conference 2020.</td>
             </tr>
             <tr>
-              <td class="border-r px-4 py-2">Reference:</td>
-              <td class="px-4 py-2" colspan="2">{{ $user->payment->paymentId }}</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Amount:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">{{ $user->payment->amount }} INR (Registration Fee)</td>
+            </tr>
+            <tr>
+              <td class="border-red-500 border-dashed border-b px-4 py-2">Reference:</td>
+              <td class="border-red-500 border-dashed border-b px-4 py-2" >{{ $user->payment->paymentId }}</td>
             </tr>
           </tbody>
         </table>
