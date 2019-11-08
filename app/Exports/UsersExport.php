e5a5ef1f466c60ab85554e30ec7af08331a20959
Jaikhlang Brahma <jaikhlang@gmail.com>
2019-11-08 08:24:54 +0530

    export

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -12,6 +12,11 @@ class UsersExport implements FromCollection
     */
     public function collection()
     {
-        return User::all();
+        $users = User::where('status', 'UNPAID')->exclude(['image_url', 'regno', 'address', 'subscription'])->get();
+        foreach($users as $key => $user){
+          $user->cat = $user->category->name;
+          // $user->amount = $user->payment->amount;
+        }
+        return $users;
     }
 }
