e40828abde3fe0a7b2635b372ceace39f271d326
Jaikhlang Brahma <jaikhlang@gmail.com>
2019-11-09 18:45:58 +0530

    added meta tags

---
+++ resources/views/layouts/partials/partner.blade.php
@@ -0,0 +1,18 @@
+<div id="organiser" class="d-none organiser py-5 bg-white border-top border-light">
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
+        <img src="{{ asset('images/logo1.jpg') }}" alt="">
+      </a>
+      <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
+        <img src="{{ asset('images/logo2.jpg') }}" alt="">
+      </a>
+      <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
+        <img src="{{ asset('images/logo3.jpg') }}" alt="">
+      </a>
+      <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
+        <img src="{{ asset('images/logo4.jpg') }}" alt="">
+      </a>
+    </div>
+  </div>
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -162,27 +162,6 @@
         </div>
       </div>
     </div>
-
-
-
-    <div id="organiser" class="d-none organiser py-5 bg-white border-top border-light">
-      <div class="container">
-        <div class="d-flex justify-content-center">
-          <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
-            <img src="{{ asset('images/logo1.jpg') }}" alt="">
-          </a>
-          <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
-            <img src="{{ asset('images/logo2.jpg') }}" alt="">
-          </a>
-          <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
-            <img src="{{ asset('images/logo3.jpg') }}" alt="">
-          </a>
-          <a href="#" class="d-block text-decoration-none flex-item px-2 py-2">
-            <img src="{{ asset('images/logo4.jpg') }}" alt="">
-          </a>
-        </div>
-      </div>
-    </div>
     @include('layouts.partials.footer')
   </section>
 @endsection
