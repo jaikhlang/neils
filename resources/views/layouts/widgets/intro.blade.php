30e300585098b9c16a0596a11b56383b3aead908
Sangrang Brahma <jaikhlang@gmail.com>
2022-10-29 15:56:28 +0530

    updating payment

--- resources/views/layouts/widgets/intro.blade.php
+++ resources/views/layouts/widgets/intro.blade.php
@@ -1,13 +1,5 @@
 <div class="col-md-12 bg-dark text-white text-center py-2">
+  <span class="mr-2">REGISTRATION FOR NEILS12 WILL OPEN ON NOVEMBER 1ST 2022</span>
 
-  <?php
-      $open = date_format(date_create('2022-11-01'),"Y-m-d");
-      $today = date_format(date_create(now()),"Y-m-d");
-  ?>
 
-      @if($today >= $open)
-        <span class="mr-2">REGISTRATION FOR NEILS12 IS OPEN</span>
-      @else
-        <span class="mr-2">REGISTRATION FOR NEILS12 WILL OPEN ON NOVEMBER 1ST 2022</span>
-      @endif
 </div>
